5f09ea3616157d0bb6c20afd065f32ce4a53c7dc
NoahCalderwood <noahcalderwood3@gmail.com>
2024-05-07 15:15:16 -0500

    added some html base and created a working modal

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+const petForm = document.querySelector("#petForm");
+const petBtn = document.querySelector("#petBtn");
+const petInfo = document.querySelector("#petInfo");
+const closeModal = document.querySelector("#close");
+
+
+
+petBtn.addEventListener("click", () => {
+    petForm.showModal();
+  });
+
+closeModal.addEventListener("click", (event) => {
+    event.preventDefault();
+    petForm.close();
+});
+
+petInfo.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+});

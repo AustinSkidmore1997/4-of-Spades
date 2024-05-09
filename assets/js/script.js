d0a58ad656b00fdeb31843117f0f37f722782151
NoahCalderwood <noahcalderwood3@gmail.com>
2024-05-09 11:47:50 -0500

    added javascript for weather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,24 +6,51 @@ const closeModal = document.querySelector("#close");
 
 
 petBtn.addEventListener("click", () => {
-    petForm.showModal();
-  });
+  petForm.showModal();
+});
 
 closeModal.addEventListener("click", (event) => {
-    event.preventDefault();
-    petForm.close();
+  event.preventDefault();
+  petForm.close();
 });
 
 petInfo.addEventListener("submit", (event) => {
-    event.preventDefault();
-    
+  event.preventDefault();
+
 });
 
 function getWeatherApi() {
-    const weatherApi =  "https://api.weather.gov/alerts/active"
-fetch(weatherApi)
-.then(function (response) {
-  return response.json();
-})
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+
+
+      fetch(`https://api.weather.gov/points/${latitude},${longitude}`, {
+        headers: {
+          "User-Agent": "(noahcalderwood3@gmail.com)",
+        },
+      })
+        .then((result) => result.json())
+        .then((json) => {
+          //renderfunc(json);
+          console.log(json);
+          const forecast = json.properties.forecast;
+          fetch(`${forecast}`, {
+            headers: {
+              "User-Agent": "(noahcalderwood3@gmail.com)",
+            },
+          })
+            .then((result) => result.json())
+            .then((json) => {
+              console.log(json);
+            }
+            )
+        });
+    },
+    function () {
+      console.error("some error occured");
+    }
+  );
 }
 getWeatherApi();
